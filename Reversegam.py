1c8d250ed8e04030c9541705efb7e53c04187fe5
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2019-09-03 00:53:41 -0700

    Init

---
+++ Reversegam.py
@@ -0,0 +1,97 @@
+import random
+import sys
+WIDTH = 8
+HEIGHT = 8
+
+
+def draw_board(board):
+    print('  12345678')
+    print(' +--------+')
+    for y in range(HEIGHT):
+        print('%s|' % (y+1), end='')
+        for x in range(WIDTH):
+            print(board[x][y], end='')
+        print('|%s' % (y+1))
+    print(' +--------+')
+    print('  12345678')
+
+
+def get_new_board():
+    board = []
+    for i in range(WIDTH):
+        board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+    return board
+
+
+def is_valid_move(board, tile, x_start, y_start):
+    if board[x_start][y_start] != ' ' or not is_on_board(x_start, y_start):
+        return False
+
+    if tile == 'X':
+        other_tile = 'O'
+    else:
+        other_tile = 'X'
+
+    tiles_to_flip = []
+
+    for x_direction, y_direction in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+        x, y = x_start, y_start
+        x += x_direction
+        y += y_direction
+        while is_on_board(x, y) and board[x][y] == other_tile:
+            # Keep moving in this direction
+            x += x_direction
+            y += y_direction
+            if is_on_board(x, y) and board[x][y] == tile:
+                # There are pieces to flip
+                while True:
+                    x -= x_direction
+                    y -= y_direction
+                    if x == x_start and y == y_start:
+                        break
+                    tiles_to_flip.append([x, y])
+
+    if len(tiles_to_flip) == 0:
+        return False
+    return tiles_to_flip
+
+
+def is_on_board(x, y):
+    return 0 <= x <= (WIDTH - 1) and 0 <= y <= (HEIGHT - 1)
+
+
+def get_board_with_valid_moves(board, tile):
+    board_copy = get_board_copy(board)
+
+    for x, y in get_valid_moves(board_copy, tile):
+        board_copy[x][y] = '.'
+    return board_copy
+
+
+def get_valid_moves(board, tile):
+    valid_moves = []
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            if is_valid_move(board, tile, x, y):
+                valid_moves.append([x, y])
+    return valid_moves
+
+
+def get_score_of_board(board):
+    x_score = 0
+    o_score = 0
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            if board[x][y] == 'X':
+                x_score += 1
+            if board[x][y] == 'Y':
+                o_score += 1
+    return {'X': x_score, 'O': o_score}
+
+
+def get_board_copy(board):
+    board_copy = get_new_board()
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            board_copy[x][y] = board[x][y]
+    return board_copy
